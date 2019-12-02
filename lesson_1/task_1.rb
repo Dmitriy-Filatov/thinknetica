e1fa376b62c274c157ccd75b1a1c59792e700fd4
Your Name <your_email@whatever.com>
2019-12-02 22:24:16 +0300

    fixes

--- lesson_1/task_1.rb
+++ lesson_1/task_1.rb
@@ -1,9 +1,9 @@
-print "Как Вас зовут?"
+print 'Как Вас зовут?'
 name = gets.chomp.capitalize
-print "Какой Ваш рост (см.)?"
-height = Integer(gets.chomp)
+print 'Какой Ваш рост (см)?'
+height = gets.chomp.to_i
 if height > 0
   puts "Ваш идеальный вес #{(height - 110)*1.15} кг, #{name}."
 else
-  puts "Ваш вес уже оптимльный."
+  puts 'Ваш вес уже оптимльный.'
 end
