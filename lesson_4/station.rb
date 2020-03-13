c2685d500e0a375eb0b94caff627fe4f594955be
Dmitriy <dmitriy@MacBook-Pro-Dmitriy.local>
2020-03-14 00:33:54 +0300

    lesson_4 step 2 class method all

--- lesson_4/station.rb
+++ lesson_4/station.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class Station
+  @all = []
+
+  class << self
+    attr_reader :all
+
+    def all=(object)
+      @all << object
+    end
+  end
+
   attr_reader :name, :trains
 
   def initialize(name)
     @name = name
     @trains = []
+    self.class.all = self
   end
 
   def add_train(train)
